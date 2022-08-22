e0334090828a4c335bf28e9e1e16d42e273184bb
minh nguyen <hichic375@gmail.com>
2022-08-22 22:13:03 +0700

    Bài Hello World

---
+++ Code_C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //hiển thị ra màn hình
+            Console.Write("Hello world!");
+
+            Console.ReadLine();
+        }
+    }
+}
